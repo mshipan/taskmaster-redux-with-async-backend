de62a78ee66a34b23d6d5fe57e796d92fe6bf0ed
mshipan <mshipan657@gmail.com>
2023-10-20 13:31:37 +0600

    handle post mutation

--- src/components/tasks/TaskCard.jsx
+++ src/components/tasks/TaskCard.jsx
@@ -1,29 +1,36 @@
-import { ArrowRightIcon, TrashIcon } from '@heroicons/react/24/outline';
-import { useDispatch } from 'react-redux';
-import {
-  removeTask,
-  updateStatus,
-} from '../../redux/features/tasks/tasksSlice';
+import { ArrowRightIcon, TrashIcon } from "@heroicons/react/24/outline";
+import { useUpdateTaskMutation } from "../../redux/features/api/baseApi";
 
 const TaskCard = ({ task }) => {
-  const dispatch = useDispatch();
-
+  // const dispatch = useDispatch();
+  const [updateTask, { data }] = useUpdateTaskMutation();
+  console.log(data);
+  const handleUpdate = (id, updatedStatus) => {
+    const data = {
+      status: updatedStatus,
+    };
+    const options = {
+      id: id,
+      data: data,
+    };
+    updateTask(options);
+  };
   let updatedStatus;
 
-  if (task.status === 'pending') {
-    updatedStatus = 'running';
-  } else if (task.status === 'running') {
-    updatedStatus = 'done';
+  if (task.status === "pending") {
+    updatedStatus = "running";
+  } else if (task.status === "running") {
+    updatedStatus = "done";
   } else {
-    updatedStatus = 'archive';
+    updatedStatus = "archive";
   }
   return (
     <div className="bg-secondary/10 rounded-md p-5">
       <h1
         className={`text-lg font-semibold mb-3 ${
-          task.priority === 'high' ? 'text-red-500' : ' '
-        } ${task.priority === 'medium' ? 'text-yellow-500' : ' '} ${
-          task.priority === 'low' ? 'text-green-500' : ' '
+          task.priority === "high" ? "text-red-500" : " "
+        } ${task.priority === "medium" ? "text-yellow-500" : " "} ${
+          task.priority === "low" ? "text-green-500" : " "
         }`}
       >
         {task?.title}
@@ -33,13 +40,11 @@ const TaskCard = ({ task }) => {
       <div className="flex justify-between mt-3">
         <p>{task?.date}</p>
         <div className="flex gap-3">
-          <button onClick={() => dispatch(removeTask(task.id))} title="Delete">
+          <button onClick={() => console.log("remove")} title="Delete">
             <TrashIcon className="h-5 w-5 text-red-500" />
           </button>
           <button
-            onClick={() =>
-              dispatch(updateStatus({ id: task.id, status: updatedStatus }))
-            }
+            onClick={() => handleUpdate(task._id, updatedStatus)}
             title="Update Status"
           >
             <ArrowRightIcon className="h-5 w-5 text-primary" />
